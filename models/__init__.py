d58cec886dd5daf669dfdf02ad0d86fe167db1ab
jiaweiru <13605580719@163.com>
2023-05-09 09:58:27 +0800

    RI+Mel Loss QAQ

--- models/__init__.py
+++ models/__init__.py
@@ -3,4 +3,4 @@ from .ARN import ARN
 from .TFNet import TFNet
 from .MPARN import MPARN
 
-from .loss import magri_loss, commit_loss, ri_loss, tf_loss, magri_feature_loss, commit_loss_magphase, magri_multimel_loss
+from .loss import magri_loss, commit_loss, ri_loss, tf_loss, magri_feature_loss, commit_loss_magphase, magri_multimel_loss, ri_multimel_loss
